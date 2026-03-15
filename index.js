fc409bc10b0efe910ae9bf9f9c8828deb54e32c5
ToiHanEm <dinhthanhminhk11@gmail.com>
2026-03-15 22:34:31 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const path = require('path');
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, Rosita Madlife!');
 });
 
-app.use('/BrainrotFilter', express.static('BrainrotFilter'));
+app.use('/SpitalBetty', express.static(path.join(__dirname, 'SpitalBetty')));
 
 app.listen(port, () => {
   console.log(`Server run in port http://localhost:${port}`);
